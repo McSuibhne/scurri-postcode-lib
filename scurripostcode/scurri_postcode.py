6a61002c0de6bb7c7002df5f634e7d7c09ea01ff
mcsuibhne <jonathan.sweeney@ucdconnect.ie>
2022-04-10 19:25:28 +0100

    Implemented regex postcode validation - all tests passing

--- scurripostcode/scurri_postcode.py
+++ scurripostcode/scurri_postcode.py
@@ -1,3 +1,13 @@
+import re
+
+# Regex from: https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
+POSTCODE_REGEX = '^(([A-Z]{1,2}[0-9][A-Z0-9]' \
+                            '?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ' \
+                            '?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]' \
+                            '?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$'
+
+COMPILED_REGEX = re.compile(POSTCODE_REGEX)
+
 
 def validate_postcode(postcode: str) -> bool:
     """
@@ -7,5 +17,8 @@ def validate_postcode(postcode: str) -> bool:
     Input: an unverified postcode as a string.
     Output: a boolean.
     """
+    if not COMPILED_REGEX.match(postcode.upper().replace(" ","").replace("-","")):
+        return False
+    else:
+        return True
 
-    return False #Intitial value to produce failing unit test
