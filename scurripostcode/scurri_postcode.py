ba49429a698e94ab467f442f735ba269507c1661
mcsuibhne <jonathan.sweeney@ucdconnect.ie>
2022-04-10 19:40:08 +0100

    Implemented postcode formatting function - all tests passing

--- scurripostcode/scurri_postcode.py
+++ scurripostcode/scurri_postcode.py
@@ -9,16 +9,41 @@ POSTCODE_REGEX = '^(([A-Z]{1,2}[0-9][A-Z0-9]' \
 COMPILED_REGEX = re.compile(POSTCODE_REGEX)
 
 
+class InvalidPostcode(Exception):
+    """Raised when an invalid code is
+    passed to the format function"""
+    pass
+
+
 def validate_postcode(postcode: str) -> bool:
     """
     Determines whether a given string's
     characters are in valid UK postcode
     structure, regardless of format.
     Input: an unverified postcode as a string.
-    Output: a boolean.
+    Output: a boolean value.
     """
     if not COMPILED_REGEX.match(postcode.upper().replace(" ","").replace("-","")):
         return False
     else:
         return True
 
+
+def format_postcode(postcode: str) -> str:
+    """
+    Receives a valid UK postcode string
+    and returns the code in standard format.
+    Input: a UK postcode as a string.
+    Output: a correctly formatted UK postcode
+    as a string.
+    """
+
+    if validate_postcode(postcode) == False:
+        raise InvalidPostcode()
+
+    postcode = postcode.upper().replace(' ', '').replace('-', '')
+
+    inward_code = postcode[-3:]
+    outward_code = postcode[:-3]
+
+    return outward_code + " " + inward_code
